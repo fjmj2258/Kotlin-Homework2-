e8e17283a830e328e80547368b11055e009a60fe
fjmj2258 <fjmj2258@gmail.com>
2022-11-10 13:13:17 +0800

    HW

---
+++ Lab6/app/src/main/java/com/example/lab6_kotlin/CustomToastActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab6_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomToastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_toast)
+    }
+}
